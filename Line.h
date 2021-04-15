a0c4f6622b48d363e7b40ab645f147b5c6f69cdc
krehwell <fahrimuham001@gmail.com>
2021-04-16 06:10:47 +0800

    add line wrapper

---
+++ Line.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "memake.h"
+
+class Line
+{
+public:
+    Line(int x1, int y1, int x2, int y2)
+    {
+        this->x1 = x1;
+        this->y1 = y1;
+        this->x2 = x2;
+        this->y2 = y2;
+    }
+
+    void Draw(SDL_Renderer* renderer,  SDL_Color color)
+    {
+        SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 1);
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    }
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+};
+
