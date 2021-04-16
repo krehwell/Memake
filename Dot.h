18e0bed575e53e866fc70392dce5de04346655b9
krehwell <fahrimuham001@gmail.com>
2021-04-17 01:15:51 +0800

    add dot wrapper

---
+++ Dot.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Memake.h"
+#include "Utils.h"
+
+class Dot
+{
+public:
+    Dot(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    void Draw(SDL_Renderer* renderer,  SDL_Color color)
+    {
+        SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);;
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
+
+    int x;
+    int y;
+};
