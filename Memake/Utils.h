c2fa8937a58edbcefd10f1e102f99697ff59b9a1
krehwell <fahrimuham00@gmail.com>
2021-07-17 22:42:26 +0800

    fix me

--- Memake/Utils.h
+++ Memake/Utils.h
@@ -1,3 +1,25 @@
+/*
+   SDL2_gfxPrimitives.c: graphics primitives for SDL2 renderers
+   Copyright (C) 2012-2014  Andreas Schiffler
+   Modifications and additions for BBC BASIC (C) 2016-2020 Richard Russell
+   This software is provided 'as-is', without any express or implied
+   warranty. In no event will the authors be held liable for any damages
+   arising from the use of this software.
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+   3. This notice may not be removed or altered from any source
+   distribution.
+   Andreas Schiffler -- aschiffler at ferzkopp dot net
+   Richard Russell -- richard at rtrussell dot co dot uk
+*/
+
 #pragma once
 #include <SDL2/SDL.h>
 
